23c4e8abd67cdf34b199b9c567a338210186ad9e
kgpartee <Katherinepartee2027@u.northwestern.edu>
2026-04-02 10:40:46 -0500

    created pico project

---
+++ HW 2/servo_sweep/servo_sweep.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+//brown ground, red power, orange signal
+// run 5V
+int main()
+{
+    stdio_init_all();
+    // initialize pwm , div 50 wrap 60000
+
+    while (true) {
+        // set index
+        // loop through degree values in set servo function
+    }
+}
+
+// write set servo with pwm, convert angle to wrap number for percent duty cycle 
