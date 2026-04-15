7f7d5dfdbc41f308b1e1483b0d9759d4f41777a2
kgpartee <Katherinepartee2027@u.northwestern.edu>
2026-04-14 21:55:58 -0500

    read and recombination functions

--- HW 5/imu_screen/imu.c
+++ HW 5/imu_screen/imu.c
@@ -8,13 +8,32 @@ void imu_init(){
     i2c_write_blocking(i2c_default, IMU_ADDR, accelbuf, 1, true);
     i2c_write_blocking(i2c_default, IMU_ADDR, gyrobuf, 1, true);
 }
-
-void read_data(){
+uint8_t read_data(uint8_t addr){
     uint8_t buf[14]; 
-    i2c_write_blocking(i2c_default, IMU_ADDR, &ACCEL_XOUT_H, 1, true);  // true to keep host control of bus
-    i2c_read_blocking(i2c_default, IMU_ADDR, &buf, 1, false);  // false - finis
+    i2c_write_blocking(i2c_default, IMU_ADDR, &addr, 1, true);  // true to keep host control of bus
+    for (int ii = 0; ii < 14; ii++){
+        i2c_read_blocking(i2c_default, IMU_ADDR, &buf+ii, 1, true);
+    }
+    return buf;  
 }
 
+int recombination(uint8_t buf[14]){
+    int data [7];
+    for (int ii = 0; ii < 13; ii+=2){
+        data[ii/2] = (buf[ii]<<8)||buf[ii+1];
+    }
+    //fix units
+    data[0] = data[0]*0.000061;
+    data[1] = data[2]*0.000061;
+    data[2] = data[3]*0.000061;
+
+    data[3] = data[3]/340+36.53;
+    data[4] = data[4]/340+36.53;
+
+    data[5] = data[5]*0.007630;
+    data[6] = data[6]*0.007630;
+    data[7] = data[7]*0.007630;
+}
 // uint8_t readPin(int pinnum){
 //     uint8_t buf = 0; 
 //     i2c_write_blocking(i2c_default, CHIPADDR, &GPIO, 1, true);  // true to keep host control of bus
