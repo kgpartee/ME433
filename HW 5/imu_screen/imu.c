ae6594817ca2f2adcee839d727233efd0ec7a10b
kgpartee <Katherinepartee2027@u.northwestern.edu>
2026-04-14 20:53:08 -0500

    initialize i2c

---
+++ HW 5/imu_screen/imu.c
@@ -0,0 +1,5 @@
+#include "imu.h"
+
+void imu_init(){
+    
+}
--- HW 5/imu_screen/imu_screen.c
+++ HW 5/imu_screen/imu_screen.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "display.h"
+#include "font.h"
+#include "imu.h"
+#include "ssd1306.h"
 
+#define I2C_PORT i2c0
+#define I2C_SDA 8
+#define I2C_SCL 9
 
 int main()
 {
     stdio_init_all();
+    // I2C Initialisation. Using it at 1700Khz.
+    i2c_init(I2C_PORT, 1700*1000);
+    
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+
+
+
+    ssd1306_setup();
+
+
+
 
     while (true) {
         printf("Hello, world!\n");
