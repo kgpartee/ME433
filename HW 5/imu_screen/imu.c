d80265d7144961225d3f04ad6aec142be31c587e
kgpartee <Katherinepartee2027@u.northwestern.edu>
2026-04-14 21:30:06 -0500

    init and read functions

--- HW 5/imu_screen/imu.c
+++ HW 5/imu_screen/imu.c
@@ -1,5 +1,23 @@
 #include "imu.h"
 
 void imu_init(){
-    
+    uint8_t pwrbuf[2] = {PWR_MGMT_1, 0x00};
+    uint8_t accelbuf[2] = {ACCEL_CONFIG, 0x00}; // set value to 0 to select 2g mode
+    uint8_t gyrobuf[2] = {GYRO_CONFIG, 0x03}; // set value to 3 to select 2000 dps mode
+    i2c_write_blocking(i2c_default, IMU_ADDR, pwrbuf, 1, true);
+    i2c_write_blocking(i2c_default, IMU_ADDR, accelbuf, 1, true);
+    i2c_write_blocking(i2c_default, IMU_ADDR, gyrobuf, 1, true);
 }
+
+void read_data(){
+    uint8_t buf[14]; 
+    i2c_write_blocking(i2c_default, IMU_ADDR, &ACCEL_XOUT_H, 1, true);  // true to keep host control of bus
+    i2c_read_blocking(i2c_default, IMU_ADDR, &buf, 1, false);  // false - finis
+}
+
+// uint8_t readPin(int pinnum){
+//     uint8_t buf = 0; 
+//     i2c_write_blocking(i2c_default, CHIPADDR, &GPIO, 1, true);  // true to keep host control of bus
+//     i2c_read_blocking(i2c_default, CHIPADDR, &buf, 1, false);  // false - finished with bus
+//     return (buf>>pinnum)&1;
+// }
--- HW 5/imu_screen/imu.h
+++ HW 5/imu_screen/imu.h
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/i2c.h"
 
 #define IMU_ADDR 0b1101000
 
 // config registers
 #define CONFIG 0x1A
-#define GYRO_CONFIG 0x1B
-#define ACCEL_CONFIG 0x1C
-#define PWR_MGMT_1 0x6B
+uint8_t GYRO_CONFIG = 0x1B;
+uint8_t ACCEL_CONFIG = 0x1C;
+uint8_t PWR_MGMT_1 = 0x6B;
 #define PWR_MGMT_2 0x6C
 // sensor data registers:
-#define ACCEL_XOUT_H 0x3B
+uint8_t ACCEL_XOUT_H = 0x3B;
 #define ACCEL_XOUT_L 0x3C
 #define ACCEL_YOUT_H 0x3D
 #define ACCEL_YOUT_L 0x3E
